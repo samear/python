9710bdbedb24b7f9025774ddeca59e7001211295
sam ear <sam.ear@gmail.com>
2019-03-19 21:51:35 -0700

    new changes

--- binaryTree/myBinaryTree.py
+++ binaryTree/myBinaryTree.py
@@ -4,27 +4,27 @@ class Node(object):
         self.right = None
         self.leaf = data
 
-def inOrder(arg):
-    if arg == root:
-        inOrder(arg.left)
-        print(arg.leaf)
-        inOrder(arg.right)
+def inOrder(root):
+    if root:
+        inOrder(root.left)
+        print(root.leaf)
+        inOrder(root.right)
     else:
         print("tree doesn't exit")
 
-def preOrder(arg):
-    if arg == 'root':
-        print(arg.leaf)
-        preOrder(arg.left)
-        preOrder(arg.right)
+def preOrder(root):
+    if root:
+        print(root.leaf)
+        preOrder(root.left)
+        preOrder(root.right)
     else:
         print("tree doesn't exit")
 
-def postOrder(arg):
-    if arg == 'root':
-        postOrder(arg.left)
-        postOrder(arg.right)
-        print(arg.leaf)
+def postOrder(root):
+    if root:
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.leaf)
     else:
         print("tree doesn't exit")
 
--- binaryTree/usingMyBinaryTree.py
+++ binaryTree/usingMyBinaryTree.py
@@ -12,6 +12,7 @@ root.right = Node(5)
 root.right.left = Node(1)
 root.right.right = Node(3)
 
+
 print("inOrder")
 inOrder(root)
 print("preOrder")
