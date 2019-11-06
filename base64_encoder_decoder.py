b17d46494141f2000a6f3da72c3c4ff082b1a040
Sam Ear <samear@users.noreply.github.com>
2019-11-06 15:49:26 -0800

    Update base64_encoder_decoder.py

--- base64_encoder_decoder.py
+++ base64_encoder_decoder.py
@@ -1,27 +1,37 @@
+# This python script generate unique name (unique_value) and converts the sting to base64encoded format.
+# 
 #!/usr/bin/python
 import base64
 import random
 import time
 
-def encode(str):
-   #encoded_result=base64.b64encode(bytes(str, 'utf-8'))
-   global encoded_result
-   encoded_result=base64.b64encode(bytes(str, 'utf-8'))
+# generate unique value. this value is used for repo:name
+def unique_value():
+    time.sleep(0.000001)
+    r = random.randint(0, 10000)
+    return r + time.time()
 
-   print('encoded:', encoded_result)
+# convert to base64
+def encode(content):
+    global f
+    global encoded_result
 
+    encoded_result=base64.b64encode(bytes(content, 'utf-8'))
+    f.write(str(encoded_result) + '\n')
+
+# decode
 def decode(str):
    decoded_result=base64.b64decode(str).decode('utf-8')
 
    print('decoded:', decoded_result)
 
-#content = '{ "repo:name": "12345","repo:createDate": "2017-09-26T15:52:25Z", "astock:assetType": "template", "components": [{ "astock:profile": "H240", "dc:format": "image/jpeg", "tiff:imageWidth": 427, "tiff:imageLength": 240, "files": [{ "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/H240_1234.jpg" }, { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-irl1.s3-eu-west-1.amazonaws.com/00/00/01/23/H240_1234.jpg" } ] }, { "astock:profile": "H240", "dc:format": "image/webp", "tiff:imageWidth": 427, "tiff:imageLength": 240, "files": [ { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/H240_1234.webp" }, { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-irl1.s3-eu-west-1.amazonaws.com/00/00/01/23/H240_1234.webp" }] }, { "astock:profile": "500ST", "dc:format": "image/jpeg", "tiff:imageWidth": 500, "tiff:imageLength": 281, "files": [ { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/500ST_1234.jpg" } ] }, { "astock:profile": "PREVIEW_W1024", "dc:format": "image/jpeg", "tiff:imageWidth": 1024, "tiff:imageLength": 575, "files": [ { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/PREVIEW_W1024_1234.jpg" } ] } ], "derivatives": [ { "astock:derivativeType": "rendition", "astock:componentType": "thumbnail", "componentIndexes": [0] }, { "astock:derivativeType": "rendition", "astock:componentType": "thumbnail", "componentIndexes": [1] }, { "astock:derivativeType": "rendition", "astock:componentType": "thumbnail", "componentIndexes": [2] }, { "astock:derivativeType": "rendition", "astock:componentType": "preview", "componentIndexes": [3] } ] }'
-
-for n in range(2):
-   t = int(time.time() * 1000)
-   r1 = random.randint(0, 10000)
-   print('random number:', t + r1)
-   
-   content = '{ "repo:name": "' + str(t) + str(r1) + ',"repo:createDate": "2017-09-26T15:52:25Z", "astock:assetType": "template", "components": [{ "astock:profile": "H240", "dc:format": "image/jpeg", "tiff:imageWidth": 427, "tiff:imageLength": 240, "files": [{ "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/H240_1234.jpg" }, { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-irl1.s3-eu-west-1.amazonaws.com/00/00/01/23/H240_1234.jpg" } ] }, { "astock:profile": "H240", "dc:format": "image/webp", "tiff:imageWidth": 427, "tiff:imageLength": 240, "files": [ { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/H240_1234.webp" }, { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-irl1.s3-eu-west-1.amazonaws.com/00/00/01/23/H240_1234.webp" }] }, { "astock:profile": "500ST", "dc:format": "image/jpeg", "tiff:imageWidth": 500, "tiff:imageLength": 281, "files": [ { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/500ST_1234.jpg" } ] }, { "astock:profile": "PREVIEW_W1024", "dc:format": "image/jpeg", "tiff:imageWidth": 1024, "tiff:imageLength": 575, "files": [ { "repo:size": 424242, "repo:createDate": "2017-09-26T15:52:25Z", "repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178", "href": "s3://stock-public-thumbnails-stage-va6.s3-us-east-1.amazonaws.com/00/00/01/23/PREVIEW_W1024_1234.jpg" } ] } ], "derivatives": [ { "astock:derivativeType": "rendition", "astock:componentType": "thumbnail", "componentIndexes": [0] }, { "astock:derivativeType": "rendition", "astock:componentType": "thumbnail", "componentIndexes": [1] }, { "astock:derivativeType": "rendition", "astock:componentType": "thumbnail", "componentIndexes": [2] }, { "astock:derivativeType": "rendition", "astock:componentType": "preview", "componentIndexes": [3] } ] }'
-   encode(content)
-   decode(encoded_result)
+num_content = 1
+f = open('content_b64encoded' + '.txt', 'w')
+for n in range(num_content):
+    print('counter:', n)
+    #{"repo:name": "1", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}
+    
+    content = '{"repo:name": "' + str(unique_value()) + '", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}'
+    encode(content)
+    decode(encoded_result)
+f.close()
