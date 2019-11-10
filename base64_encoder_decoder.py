04514305e73bbffb21bbd839fd09c215d47c0355
Sam Ear <sam.ear@gmail.com>
2019-11-09 16:06:42 -0800

    cleaning code

--- base64_encoder_decoder.py
+++ base64_encoder_decoder.py
@@ -1,13 +1,13 @@
-# This python script generate unique name (unique_value) and converts the sting to base64encoded format.
-# 
+# This python script generates unique values (unique_value) and converts the sting below to base64encoded format.
+# {"repo:name": "<unique values>", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}
 #!/usr/bin/python
+
 import base64
 import random
 import time
-from timeit import default_timer as timer
 from datetime import timedelta
 
-#result_folder = '/Users/samear/apache-jmeter-5.1.1/projects/repo-man/data/'
+# result_folder = '/Users/samear/apache-jmeter-5.1.1/projects/repo-man/data/'
 result_folder = '/home/samear/python/data/'
 
 # generate unique value. this value is used for repo:name
@@ -35,44 +35,43 @@ def encode_Renditions(content):
 
 # decode
 def decode(str):
-   decodedStr=base64.b64decode(str).decode('utf-8')
+    decodedStr=base64.b64decode(str).decode('utf-8')
 
-   print('decoded:', decodedStr)
+    print('decoded:', decodedStr)
 
-# record asset name to a file
+# create unique value for asset name to a file
 def record_assetName(name):
-   global f3
+    global f3
    
-   f3.write(name +'\n')
+    f3.write(name +'\n')
 
 start = time.time()
-num_content = 10
+num_content = 1000
 f1 = open(result_folder + 'assets' + '.txt', 'w')
 f2 = open(result_folder + 'renditions' + '.txt', 'w')
 f3 = open(result_folder + 'assetName' + '.txt', 'w')
 
 for n in range(num_content):
-   assetName = str(unique_value())
-   renditionName = str(unique_value())
+    assetName = str(unique_value())
+    renditionName = str(unique_value())
    
-   record_assetName(assetName)
-   record_assetName(renditionName)
+    record_assetName(assetName)
+    record_assetName(renditionName)
 
-   print('Generating...', n + 1, 'Asset:', assetName, '- Rendition:', renditionName)
-   #{"repo:name": "1", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}
-    
-   asset_data = '{"repo:name": "' + assetName + '", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}'
-   encode_Assets(asset_data)
+    print('Generating...', n + 1, 'Asset:', assetName, '- Rendition:', renditionName)
 
-   rendition_data = '{"repo:name": "' + renditionName + '", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}'
-   encode_Renditions(rendition_data)
+    # Creating content for /newAsset transaction 
+    asset_data = '{"repo:name": "' + assetName + '", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}'
+    encode_Assets(asset_data)
 
-   #decode(encodedStr)
+    # Creating content for /newRenditiontransaction
+    rendition_data = '{"repo:name": "' + renditionName + '", "repo:createDate": "2017-09-26T15:52:25Z","astock:assetType": "vector","components": [{"astock:profile": "zipVector","dc:format": "application/vnd.adobe.stock.vec+zip","files": [{"repo:size": 424242,"repo:createDate": "2017-09-26T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2178","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/zipVector_1234.zip"}]},{"astock:profile": "vector","dc:format": "application/postscript","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"tiff:XResolution": {"tiff:numerator": 300,"tiff:denominator": 1},"files": [{"repo:size": 424243,"repo:createDate": "2017-09-27T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2179","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/vector_1234.ai"}]},{"astock:profile": "image","dc:format": "image/jpeg","tiff:imageWidth": 2048,"tiff:imageLength": 1152,"files": [{"repo:size": 424244,"repo:createDate": "2017-09-28T15:52:25Z","repo:etag": "95b9b5e299d60bf4e5972d61ef6d2180","href": "s3://fotolia-stage-vectors.s3-eu-west-1.amazonaws.com/00/00/01/23/image_1234.jpg"}]}],"derivatives": [{"astock:derivativeType": "original","astock:componentType": "primary","componentIndexes": [0]},{"astock:derivativeType": "original","astock:componentType": "zipEntry","componentIndexes": [1, 2]},{"astock:derivativeType": "deliverable","astock:componentType": "primary","componentIndexes": [1]}]}'
+    encode_Renditions(rendition_data)
+
+    #decode(encodedStr)
 f1.close()
 f2.close()
 f3.close()
 
 end = time.time()
-#elapsed_time = time.process_time() - t
 print(timedelta(seconds=end-start))
-#print('Elapsed time:', elapsed_time)
