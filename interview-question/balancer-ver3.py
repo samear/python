f70961cd1dfac5bee181348f85e84ee16f4b6659
Sam Ear <sam.ear@gmail.com>
2019-11-17 22:08:56 -0800

    latest

--- interview-question/balancer-ver3.py
+++ interview-question/balancer-ver3.py
@@ -19,6 +19,8 @@ i/p -> {([])}: valid
 i/p -> {[)}(]: not valid
 i/p -> ;{}: not valid
 """
+flag = True
+
 def balance(arr):
 
     pair1 = ['(', ')']
@@ -29,7 +31,7 @@ def balance(arr):
 
     #print('length of arr:', len(arr))
     x = int(len(arr)/2)
-    #y = x-1
+    
 
     #print('length:', x)
     #print(arr[x])
@@ -40,20 +42,35 @@ def balance(arr):
         #print(x+n, '=', arr[x+n], end=' - ') # print the second half of the array
 
         if arr[x-1-n] == pair1[0] and arr[x+n] == pair1[1]:
-            print(arr[x-1-n] + arr[x+n], '- Valid a') # pair of ()
+            #print(arr[x-1-n] + arr[x+n], '- Valid a') # pair of ()
+            flag = True
+            display_result(flag)
         elif arr[x-1-n] == pair2[0] and arr[x+n] == pair2[1]: 
-            print(arr[x-1-n] + arr[x+n], '- Valid b') # pair of {}
+            #print(arr[x-1-n] + arr[x+n], '- Valid b') # pair of {}
+            flag = True
+            display_result(flag)
         elif arr[x-1-n] == pair3[0] and arr[x+n] == pair3[1]:
-            print(arr[x-1-n] + arr[x+n], '- Valid c') # pair of []
+            #print(arr[x-1-n] + arr[x+n], '- Valid c') # pair of []
+            flag = True
+            display_result(flag)
 
         elif arr[n+index1] == pair1[0] and arr[n+index2] == pair1[1]:
-            print(arr[n+index1] + arr[n+index2], '- Valid x') # pair of ()
+            #print(arr[n+index1] + arr[n+index2], '- Valid x') # pair of ()
+            flag = True
+            display_result(flag)
         elif arr[n+index1] == pair2[0] and arr[n+index2] == pair2[1]:
             print(arr[n+index1] + arr[n+index2], '- Valid y') # pair of {}
+            flag = True
+            display_result(flag)
         elif arr[n+index1] == pair3[0] and arr[n+index2] == pair3[1]:
-            print(arr[n+index1] + arr[n+index2], '- Valid z') # pair of []
+            #print(arr[n+index1] + arr[n+index2], '- Valid z') # pair of []
+            flag = True
+            display_result(flag)
         else:
-            print('Not valid')
+            #print('Not valid')
+            flag = False
+            display_result(flag)
+
         index1 = index1 + 1
         index2 = index1 + 1
         #print('index1 =', index1)
@@ -61,18 +78,24 @@ def balance(arr):
         #print('[',x-1-n,']:',arr[x-1-n], end='')
         #print(arr[x-1-n], end='')
 
-x = ['{','}','[',']','{','}']
-"""
-Test cases:
-i/p -> Empty string: valid
-i/p -> (): valid
-i/p -> []: valid
-i/p -> (){}: valid
-i/p -> {]: Not valid
-i/p -> ([)]: Not valid
-i/p -> {([])}: valid
-i/p -> {[)}(]: not valid
-i/p -> ;{}: not valid
-"""
+def display_result(f):
+    if f == False:
+        print('Valid:', f)
+        exit
+    else:
+        print('Valid:', f)
+
+#display_result(flag)
+
+# Test cases:
+#x = ['',''] # not valid
+#x = ['(',')'] # valid
+#x = ['[',']'] # valid
+#x = ['(',')','{','}'] # valid
+#x = ['{','}'] # not valid
+#x = ['(','[',')',']'] # not valid
+#x = ['{','(','[',']',')','}'] # valid
+#x = ['{','[',')','}','(',']'] # not valid
+x = [';','{','}',':'] # not valid
 
 balance(x)
